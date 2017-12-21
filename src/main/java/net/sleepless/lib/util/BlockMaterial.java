6b2045a35c78db4ec30fe811763a371ee1cafaf3
InsomniaKitten <admin@designdevils.co.uk>
2017-12-21 17:57:22 +0000

    Add BlockMaterial and ColorVariant enums

---
+++ src/main/java/net/sleepless/lib/util/BlockMaterial.java
@@ -0,0 +1,63 @@
+package net.sleepless.lib.util;
+
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.util.IStringSerializable;
+
+import java.util.Locale;
+
+public enum BlockMaterial implements IStringSerializable {
+
+    CARPET(Material.CARPET, SoundType.CLOTH, 0.1F, 0.5F),
+    CIRCUITS(Material.CIRCUITS, SoundType.STONE, 0.0F, 0.0F),
+    CLOTH(Material.CLOTH, SoundType.CLOTH, 0.8F, 4.0F),
+    GLASS(Material.GLASS, SoundType.GLASS, 0.3F, 1.5F),
+    GLOWSTONE(Material.GLASS, SoundType.GLASS, 0.3F, 1.5F),
+    GRASS(Material.GRASS, SoundType.PLANT, 0.6F, 3F),
+    GROUND(Material.GROUND, SoundType.GROUND, 0.5F, 2.5F),
+    ICE(Material.ICE, SoundType.GLASS, 0.5F, 2.5F),
+    LADDER(Material.WOOD, SoundType.LADDER, 0.4F, 2.0F),
+    LEAVES(Material.LEAVES, SoundType.PLANT, 0.2F, 1.0F),
+    METAL(Material.IRON, SoundType.METAL, 5.0F, 30.0F),
+    PLANT(Material.PLANTS, SoundType.PLANT, 0.0F, 0.0F),
+    QUARTZ(Material.ROCK, SoundType.STONE, 0.8F, 4.0F),
+    ROCK(Material.ROCK, SoundType.STONE, 1.5F, 30.0F),
+    SAND(Material.SAND, SoundType.SAND, 0.5F, 2.5F),
+    SNOW(Material.SNOW, SoundType.SNOW, 0.2F, 1.0F),
+    VINE(Material.VINE, SoundType.PLANT, 0.2F, 1.0F),
+    WOOD(Material.WOOD, SoundType.WOOD, 2.0F, 15.0F);
+
+    private final Material material;
+    private final SoundType sound;
+    private final float hardness;
+    private final float resistance;
+
+    BlockMaterial(Material material, SoundType sound, float hardness, float resistance) {
+        this.material = material;
+        this.sound = sound;
+        this.hardness = hardness;
+        this.resistance = resistance;
+    }
+
+    public Material getMaterial() {
+        return material;
+    }
+
+    public SoundType getSound() {
+        return sound;
+    }
+
+    public float getHardness() {
+        return hardness;
+    }
+
+    public float getResistance() {
+        return resistance;
+    }
+
+    @Override
+    public String getName() {
+        return name().toLowerCase(Locale.ROOT);
+    }
+
+}
---
+++ src/main/java/net/sleepless/lib/util/ColorVariant.java
@@ -0,0 +1,57 @@
+package net.sleepless.lib.util;
+
+import net.minecraft.block.material.MapColor;
+import net.minecraft.item.EnumDyeColor;
+import net.minecraft.util.IStringSerializable;
+
+import java.util.Locale;
+
+public enum ColorVariant implements IStringSerializable {
+
+    BLACK(MapColor.BLACK, EnumDyeColor.BLACK, 0x1D1D21),
+    RED(MapColor.RED, EnumDyeColor.RED, 0xB02E26),
+    GREEN(MapColor.GREEN, EnumDyeColor.GREEN, 0x5E7C16),
+    BROWN(MapColor.BROWN, EnumDyeColor.BROWN, 0x835432),
+    BLUE(MapColor.BLUE, EnumDyeColor.BLUE, 0x3C44AA),
+    PURPLE(MapColor.PURPLE, EnumDyeColor.PURPLE, 0x8932B8),
+    CYAN(MapColor.CYAN, EnumDyeColor.CYAN, 0x169C9C),
+    LIGHT_GRAY(MapColor.SILVER, EnumDyeColor.SILVER, 0x9D9D97),
+    GRAY(MapColor.GRAY, EnumDyeColor.GRAY, 0x474F52),
+    PINK(MapColor.PINK, EnumDyeColor.PINK, 0xF38BAA),
+    LIME(MapColor.LIME, EnumDyeColor.LIME, 0x80C71F),
+    YELLOW(MapColor.YELLOW, EnumDyeColor.YELLOW, 0xFED83D),
+    LIGHT_BLUE(MapColor.LIGHT_BLUE, EnumDyeColor.LIGHT_BLUE, 0x3AB3DA),
+    MAGENTA(MapColor.MAGENTA, EnumDyeColor.MAGENTA, 0xC74EBD),
+    ORANGE(MapColor.GOLD, EnumDyeColor.ORANGE, 0xF9801D),
+    WHITE(MapColor.SNOW, EnumDyeColor.WHITE, 0xF9FFFE);
+
+    public static final ColorVariant[] VALUES = ColorVariant.values();
+
+    private final MapColor mapColor;
+    private final EnumDyeColor dyeColor;
+    private final int colorValue;
+
+    ColorVariant(MapColor mapColor, EnumDyeColor dyeColor, int colorValue) {
+        this.mapColor = mapColor;
+        this.dyeColor = dyeColor;
+        this.colorValue = colorValue;
+    }
+
+    public MapColor getMapColor() {
+        return mapColor;
+    }
+
+    public EnumDyeColor getDyeColor() {
+        return dyeColor;
+    }
+
+    public int getColorValue() {
+        return colorValue;
+    }
+
+    @Override
+    public String getName() {
+        return name().toLowerCase(Locale.ROOT);
+    }
+
+}
