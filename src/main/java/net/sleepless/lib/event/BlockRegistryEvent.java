a3ace968ecff13bfe10178e33b22d1160881d31f
InsomniaKitten <admin@designdevils.co.uk>
2017-12-21 13:02:53 +0000

    Add various helper fixes - Set unlocalized name for blocks and items if not present - Attempt to set registry name for ItemBlocks if not present

--- src/main/java/net/sleepless/lib/event/BlockRegistryEvent.java
+++ src/main/java/net/sleepless/lib/event/BlockRegistryEvent.java
@@ -1,12 +1,33 @@
 package net.sleepless.lib.event;
 
 import net.minecraft.block.Block;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.registries.IForgeRegistry;
+import net.sleepless.lib.util.ModContainerUtil;
 
 public final class BlockRegistryEvent extends ForgeRegistryEvent<Block> {
 
     protected BlockRegistryEvent(IForgeRegistry<Block> registry) {
         super(registry);
+    }
+
+    @Override
+    public void register(Block entry, ResourceLocation name) {
+        if ("block.null".equals(entry.getUnlocalizedName())) {
+            String modid = name.getResourceDomain();
+            String path = name.getResourcePath();
+            entry.setUnlocalizedName(modid + "." + path);
+        }
+        super.register(entry, name);
+    }
+
+    @Override
+    public void register(Block entry, String name) {
+        if ("block.null".equals(entry.getUnlocalizedName())) {
+            String modid = ModContainerUtil.getActiveModId();
+            entry.setUnlocalizedName(modid + "." + name);
+        }
+        super.register(entry, name);
     }
 
     public static final class Post extends ForgeRegistryEvent.Post<Block> {
--- src/main/java/net/sleepless/lib/event/ItemRegistryEvent.java
+++ src/main/java/net/sleepless/lib/event/ItemRegistryEvent.java
@@ -1,12 +1,45 @@
 package net.sleepless.lib.event;
 
+import net.minecraft.block.Block;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.registries.IForgeRegistry;
+import net.sleepless.lib.SleeplessLib;
+import net.sleepless.lib.util.ModContainerUtil;
 
 public final class ItemRegistryEvent extends ForgeRegistryEvent<Item> {
 
     protected ItemRegistryEvent(IForgeRegistry<Item> registry) {
         super(registry);
+    }
+
+    @Override
+    public void register(Item entry) {
+        if (entry instanceof ItemBlock && entry.getRegistryName() == null) {
+            SleeplessLib.LOGGER.warn("Registry name for ItemBlock was null, attempting to inherit name from Block parent.");
+            entry.setRegistryName(Block.getBlockFromItem(entry).getRegistryName());
+        }
+        super.register(entry);
+    }
+
+    @Override
+    public void register(Item entry, ResourceLocation name) {
+        if ("item.null".equals(entry.getUnlocalizedName())) {
+            String modid = name.getResourceDomain();
+            String path = name.getResourcePath();
+            entry.setUnlocalizedName(modid + "." + path);
+        }
+        super.register(entry, name);
+    }
+
+    @Override
+    public void register(Item entry, String name) {
+        if ("item.null".equals(entry.getUnlocalizedName())) {
+            String modid = ModContainerUtil.getActiveModId();
+            entry.setUnlocalizedName(modid + "." + name);
+        }
+        super.register(entry, name);
     }
 
     public static final class Post extends ForgeRegistryEvent.Post<Item> {
