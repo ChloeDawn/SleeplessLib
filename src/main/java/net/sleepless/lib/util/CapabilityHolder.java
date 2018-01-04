6545429c16b412b1d88e60d00f48a4f27669d236
InsomniaKitten <admin@designdevils.co.uk>
2018-01-04 14:52:53 +0000

    Update CapabilityHolder and add Builder system

--- src/main/java/net/sleepless/lib/util/CapabilityHolder.java
+++ src/main/java/net/sleepless/lib/util/CapabilityHolder.java
@@ -22,44 +22,63 @@ public final class CapabilityHolder {
                     capability.getName(), side.getName()
             );
 
-    private final Map<Capability<?>, Supplier<?>> globalCapabilities = new LinkedHashMap<>();
-    private final Map<EnumFacing, Map<Capability<?>, Supplier<?>>> sidedCapabilities = new LinkedHashMap<>();
+    private final CapabilityMap global;
+    private final SidedCapabilityMap sided;
 
-    private CapabilityHolder() {}
-
-    public static CapabilityHolder create() {
-        return new CapabilityHolder();
+    private CapabilityHolder(CapabilityMap global, SidedCapabilityMap sided) {
+        this.global = global;
+        this.sided = sided;
     }
 
-    public final <V> void add(Capability<V> capability, Supplier<V> instance, @Nullable EnumFacing side) {
-        if (side == null) {
-            if (!globalCapabilities.containsKey(capability)) {
-                globalCapabilities.put(capability, instance);
-            } else throw new IllegalStateException(ERROR_GLOBAL.apply(capability));
-        } else if (!globalCapabilities.containsKey(capability)) {
-            sidedCapabilities.putIfAbsent(side, new LinkedHashMap<>());
-            if (!sidedCapabilities.get(side).containsKey(capability)) {
-                sidedCapabilities.get(side).put(capability, instance);
-            } else throw new IllegalStateException(ERROR_SIDE.apply(side, capability));
-        } else throw new IllegalStateException(ERROR_GLOBAL.apply(capability));
+    public static CapabilityHolder.Builder create() {
+        return new CapabilityHolder.Builder();
     }
 
     public final boolean test(Capability<?> capability, @Nullable EnumFacing side) {
-        return sidedCapabilities.containsKey(side) && sidedCapabilities.get(side).containsKey(capability)
-                || side == null && globalCapabilities.containsKey(capability);
+        return sided.containsKey(side) && sided.get(side).containsKey(capability)
+                || side == null && global.containsKey(capability);
     }
 
     @SuppressWarnings("unchecked")
     public final <V> V get(Capability<V> capability, @Nullable EnumFacing side) {
-        if (sidedCapabilities.containsKey(side) && sidedCapabilities.get(side).containsKey(capability)) {
-            Map<Capability<?>, Supplier<?>> capabilities = sidedCapabilities.get(side);
+        if (sided.containsKey(side) && sided.get(side).containsKey(capability)) {
+            Map<Capability<?>, Supplier<?>> capabilities = sided.get(side);
             Supplier<V> supplier = (Supplier<V>) capabilities.get(capability);
             return capability.cast(supplier.get());
-        } else if (side == null && globalCapabilities.containsKey(capability)) {
-            Supplier<V> supplier = (Supplier<V>) globalCapabilities.get(capability);
+        } else if (side == null && global.containsKey(capability)) {
+            Supplier<V> supplier = (Supplier<V>) global.get(capability);
             return capability.cast(supplier.get());
         }
         return null;
     }
+
+    public static final class Builder {
+        private final CapabilityMap global = new CapabilityMap();
+        private final SidedCapabilityMap sided = new SidedCapabilityMap();
+
+        private Builder() {}
+
+        public final <V> Builder add(Capability<V> capability, Supplier<V> instance, @Nullable EnumFacing side) {
+            if (side == null) {
+                if (!global.containsKey(capability)) {
+                    global.put(capability, instance);
+                } else throw new IllegalStateException(ERROR_GLOBAL.apply(capability));
+            } else if (!global.containsKey(capability)) {
+                sided.putIfAbsent(side, new CapabilityMap());
+                if (!sided.get(side).containsKey(capability)) {
+                    sided.get(side).put(capability, instance);
+                } else throw new IllegalStateException(ERROR_SIDE.apply(side, capability));
+            } else throw new IllegalStateException(ERROR_GLOBAL.apply(capability));
+            return this;
+        }
+
+        public CapabilityHolder build() {
+            return new CapabilityHolder(global, sided);
+        }
+    }
+
+    private static final class CapabilityMap extends LinkedHashMap<Capability<?>, Supplier<?>> {}
+
+    private static final class SidedCapabilityMap extends LinkedHashMap<EnumFacing, CapabilityMap> {}
 
 }
