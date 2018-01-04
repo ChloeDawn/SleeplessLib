a0746ea81a2292f84b94ab19977270b608caa57a
InsomniaKitten <admin@designdevils.co.uk>
2018-01-04 14:12:31 +0000

    Rename WorldHelper getter methods and wrap in Optional's

--- src/main/java/net/sleepless/lib/util/WorldHelper.java
+++ src/main/java/net/sleepless/lib/util/WorldHelper.java
@@ -4,26 +4,25 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
-import javax.annotation.Nullable;
+import java.util.Optional;
 
 public final class WorldHelper {
 
     private WorldHelper() {}
 
-    @Nullable
-    public static TileEntity getBlockEntity(World world, BlockPos pos) {
-        return getBlockEntity(world, pos, TileEntity.class);
+    public static Optional<TileEntity> getTileEntity(World world, BlockPos pos) {
+        return getTileEntity(world, pos, TileEntity.class);
     }
 
-    @Nullable
-    public static <T extends TileEntity> T getBlockEntity(World world, BlockPos pos, Class<T> clazz) {
+    @SuppressWarnings("unchecked")
+    public static <T extends TileEntity> Optional<T> getTileEntity(World world, BlockPos pos, Class<T> clazz) {
         if (isBlockLoaded(world, pos)) {
             TileEntity tile = world.getTileEntity(pos);
             if (tile != null && clazz.isAssignableFrom(tile.getClass())) {
-                return (T) tile;
+                return Optional.of((T) tile);
             }
         }
-        return null;
+        return Optional.empty();
     }
 
     public static boolean isBlockLoaded(World world, BlockPos pos) {
