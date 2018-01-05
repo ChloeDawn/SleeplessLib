425d657a9a0810cf6a84a5cb2b5334148034556a
InsomniaKitten <admin@designdevils.co.uk>
2018-01-05 12:33:51 +0000

    Add CreativeTab and TabHolder (impl todo)

---
+++ src/main/java/net/sleepless/lib/util/CreativeTab.java
@@ -0,0 +1,37 @@
+package net.sleepless.lib.util;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.function.Supplier;
+
+public class CreativeTab extends CreativeTabs {
+
+    private final String label;
+    private final Supplier<ItemStack> icon;
+
+    public CreativeTab(String label, Supplier<ItemStack> icon) {
+        super("");
+        this.label = label;
+        this.icon = icon;
+    }
+
+    public CreativeTab(Supplier<ItemStack> icon) {
+        this(ModContainerUtil.getActiveModId(), icon);
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public final String getTranslatedTabLabel() {
+        return "tab." + label + ".label";
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public final ItemStack getTabIconItem() {
+        return icon.get();
+    }
+
+}
---
+++ src/main/java/net/sleepless/lib/util/annotation/TabHolder.java
@@ -0,0 +1,18 @@
+package net.sleepless.lib.util.annotation;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * Apply this to a {@link CreativeTabs} field instance to have all registered items and blocks inserted into it.
+ * Insertion is only applicable to items and blocks that have no pre-set creative tab.
+ */
+public @interface TabHolder {
+
+    /**
+     * Modid is required if this annotation is not used in an {@link Mod} annotated class.
+     * @return The owner modid of this creative tab.
+     */
+    String modid() default "";
+
+}
