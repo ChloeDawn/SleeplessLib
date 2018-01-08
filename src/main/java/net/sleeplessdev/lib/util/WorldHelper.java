a176d721589cf6dae3a14245167a9fcdb59f6165
InsomniaKitten <admin@designdevils.co.uk>
2018-01-08 15:26:36 +0000

    Add IBlockAccess getters for block entities

--- src/main/java/net/sleeplessdev/lib/util/WorldHelper.java
+++ src/main/java/net/sleeplessdev/lib/util/WorldHelper.java
@@ -2,6 +2,7 @@ package net.sleeplessdev.lib.util;
 
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 import java.util.Optional;
@@ -9,6 +10,19 @@ import java.util.Optional;
 public final class WorldHelper {
 
     private WorldHelper() {}
+
+    public static Optional<TileEntity> getBlockEntity(IBlockAccess world, BlockPos pos) {
+        return getBlockEntity(world, pos, TileEntity.class);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T extends TileEntity> Optional<T> getBlockEntity(IBlockAccess world, BlockPos pos, Class<T> clazz) {
+        TileEntity tile = world.getTileEntity(pos);
+        if (tile != null && clazz.isAssignableFrom(tile.getClass())) {
+            return Optional.of((T) tile);
+        }
+        return Optional.empty();
+    }
 
     public static Optional<TileEntity> getBlockEntity(World world, BlockPos pos) {
         return getBlockEntity(world, pos, TileEntity.class);
