e5e25e6c4816209161340b6691e746900cfeb7e2
InsomniaKitten <admin@designdevils.co.uk>
2018-02-12 17:29:00 +0000

    Add SimpleConnectingBlock

---
+++ src/main/java/net/sleeplessdev/lib/block/SimpleConnectingBlock.java
@@ -0,0 +1,55 @@
+package net.sleeplessdev.lib.block;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+import net.minecraft.block.properties.PropertyBool;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.sleeplessdev.lib.base.BlockMaterial;
+import net.sleeplessdev.lib.base.ColorVariant;
+
+import java.util.Arrays;
+import java.util.function.Function;
+
+public abstract class SimpleConnectingBlock extends SimpleBlock {
+
+    public static final ImmutableMap<EnumFacing, PropertyBool> SIDES = Arrays.stream(EnumFacing.values())
+            .collect(Maps.toImmutableEnumMap(Function.identity(), f -> PropertyBool.create(f.getName())));
+
+    public SimpleConnectingBlock(BlockMaterial material, ColorVariant color) {
+        super(material, color);
+    }
+
+    public SimpleConnectingBlock(BlockMaterial material) {
+        super(material);
+    }
+
+    protected abstract boolean canConnectTo(EnumFacing side, IBlockAccess world, BlockPos pos);
+
+    @Override
+    @Deprecated
+    public IBlockState getActualState(IBlockState state, IBlockAccess world, BlockPos origin) {
+        BlockPos.MutableBlockPos pos = new BlockPos.MutableBlockPos(origin);
+        for (EnumFacing side : EnumFacing.VALUES) {
+            pos.move(side);
+            PropertyBool key = SIDES.get(side);
+            boolean value = canConnectTo(side, world, pos);
+            state = state.withProperty(key, value);
+            pos.move(side.getOpposite());
+        }
+        return super.getActualState(state, world, origin);
+    }
+
+    @Override
+    protected BlockStateContainer createBlockState() {
+        BlockStateContainer.Builder builder = new BlockStateContainer.Builder(this);
+        for (PropertyBool property : SIDES.values()) {
+            builder.add(property);
+        }
+        return builder.build();
+    }
+
+}
