c97f6ef27f534885bab8754cd3dcd1f118209d35
InsomniaKitten <admin@designdevils.co.uk>
2018-02-12 18:52:10 +0000

    Add block entity registration to BlockRegistryEvent (TBC)

--- src/main/java/net/sleeplessdev/lib/event/BlockRegistryEvent.java
+++ src/main/java/net/sleeplessdev/lib/event/BlockRegistryEvent.java
@@ -1,8 +1,12 @@
 package net.sleeplessdev.lib.event;
 
+import com.google.common.base.CaseFormat;
 import net.minecraft.block.Block;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.registries.IForgeRegistry;
+import net.sleeplessdev.lib.base.ModContainers;
 
 public final class BlockRegistryEvent extends ForgeRegistryEvent<Block> {
 
@@ -16,6 +20,23 @@ public final class BlockRegistryEvent extends ForgeRegistryEvent<Block> {
             entry.setUnlocalizedName(name.getResourcePath());
         }
         super.register(entry, name);
+    }
+
+    public void register(Class<? extends TileEntity> blockEntity, String key) {
+        ResourceLocation id = ModContainers.applyActiveDomain(key);
+        GameRegistry.registerTileEntity(blockEntity, id.toString());
+    }
+
+    public void register(Class<? extends TileEntity> blockEntity) {
+        String name = blockEntity.getSimpleName();
+        String key = CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, name);
+        register(blockEntity, key);
+    }
+
+    public void registerAll(Class<? extends TileEntity>... blockEntities) {
+        for (Class<? extends TileEntity> blockEntity : blockEntities) {
+            register(blockEntity);
+        }
     }
 
     public static final class Post extends ForgeRegistryEvent.Post<Block> {
