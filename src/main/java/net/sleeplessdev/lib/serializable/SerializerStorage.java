fe969ee5dce9681eced8724519c5caff3fbfbb34
InsomniaKitten <admin@designdevils.co.uk>
2018-02-13 12:59:59 +0000

    Refactor SerializerStorage to use Supplier, should resolve null issues

--- src/main/java/net/sleeplessdev/lib/serializable/SerializerStorage.java
+++ src/main/java/net/sleeplessdev/lib/serializable/SerializerStorage.java
@@ -6,12 +6,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
+import java.util.function.Supplier;
 
 public final class SerializerStorage {
 
-    private final List<ISerializable> serializers;
+    private final List<Supplier<ISerializable>> serializers;
 
-    private SerializerStorage(List<ISerializable> serializers) {
+    private SerializerStorage(List<Supplier<ISerializable>> serializers) {
         this.serializers = serializers;
     }
 
@@ -19,27 +21,31 @@ public final class SerializerStorage {
         return new SerializerStorage.Builder();
     }
 
-    public ISerializable get(int index) {
-        return serializers.get(index);
-    }
-
     public void serializeAll(NBTTagCompound nbt, boolean client) {
         if (!client) {
-            for (ISerializable s : serializers) {
-                s.serialize(nbt);
+            for (Supplier<ISerializable> s : serializers) {
+                ISerializable i = s.get();
+                Objects.requireNonNull(i);
+                i.serialize(nbt);
             }
-        } else for (ISerializable s : serializers) {
-            if (s.syncToClient()) s.serialize(nbt);
+        } else for (Supplier<ISerializable> s : serializers) {
+            ISerializable i = s.get();
+            Objects.requireNonNull(i);
+            if (i.syncToClient()) i.serialize(nbt);
         }
     }
 
     public void deserializeAll(NBTTagCompound nbt, boolean client) {
         if (!client) {
-            for (ISerializable s : serializers) {
-                s.deserialize(nbt);
+            for (Supplier<ISerializable> s : serializers) {
+                ISerializable i = s.get();
+                Objects.requireNonNull(i);
+                i.deserialize(nbt);
             }
-        } else for (ISerializable s : serializers) {
-            if (s.syncToClient()) s.deserialize(nbt);
+        } else for (Supplier<ISerializable> s : serializers) {
+            ISerializable i = s.get();
+            Objects.requireNonNull(i);
+            if (i.syncToClient()) i.deserialize(nbt);
         }
     }
 
@@ -52,21 +58,21 @@ public final class SerializerStorage {
     }
 
     public static final class Builder {
-        private final List<ISerializable> serializables = new ArrayList<>();
+        private final List<Supplier<ISerializable>> serializables = new ArrayList<>();
 
         private Builder() {}
 
-        public Builder add(ISerializable serializable) {
+        public Builder add(Supplier<ISerializable> serializable) {
             serializables.add(serializable);
             return this;
         }
 
-        public Builder addAll(ISerializable... serializables) {
+        public Builder addAll(Supplier<ISerializable>... serializables) {
             this.serializables.addAll(Arrays.asList(serializables));
             return this;
         }
 
-        public Builder addAll(Collection<ISerializable> serializables) {
+        public Builder addAll(Collection<Supplier<ISerializable>> serializables) {
             this.serializables.addAll(serializables);
             return this;
         }
