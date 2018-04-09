71eef26d4c9a39ad7cc45fbd4b4c2558609f635b
InsomniaKitten <admin@designdevils.co.uk>
2018-04-09 16:41:13 +0100

    Worlds - Improve BlockPos validation and add IBlockState getter

--- src/main/java/net/sleeplessdev/lib/world/Worlds.java
+++ src/main/java/net/sleeplessdev/lib/world/Worlds.java
@@ -1,5 +1,7 @@
 package net.sleeplessdev.lib.world;
 
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
@@ -11,14 +13,18 @@ public final class Worlds {
 
     private Worlds() {}
 
+    public static IBlockState getBlockState(IBlockAccess world, BlockPos pos) {
+        return isBlockLoaded(world, pos) ? world.getBlockState(pos) : Blocks.AIR.getDefaultState();
+    }
+
     public static Optional<TileEntity> getBlockEntity(IBlockAccess world, BlockPos pos) {
         return getBlockEntity(world, pos, TileEntity.class);
     }
 
     @SuppressWarnings("unchecked")
     public static <T extends TileEntity> Optional<T> getBlockEntity(IBlockAccess world, BlockPos pos, Class<T> clazz) {
-        if (!(world instanceof World) || isBlockLoaded ((World) world, pos)) {
-            TileEntity tile = world.getTileEntity(pos);
+        if (isBlockLoaded (world, pos)) {
+            final TileEntity tile = world.getTileEntity(pos);
             if (tile != null && clazz.isAssignableFrom(tile.getClass())) {
                 return Optional.of((T) tile);
             }
@@ -26,8 +32,16 @@ public final class Worlds {
         return Optional.empty();
     }
 
-    public static boolean isBlockLoaded(World world, BlockPos pos) {
-        return world.isValid(pos) && world.isBlockLoaded(pos);
+    public static boolean isBlockLoaded(IBlockAccess world, BlockPos pos) {
+        if (world instanceof World) {
+            final World w = (World) world;
+            return w.isValid(pos) && w.isBlockLoaded(pos);
+        }
+        return pos.getY() >= 0 && pos.getY() < 256
+                && pos.getX() >= -30000000
+                && pos.getZ() >= -30000000
+                && pos.getX() < 30000000
+                && pos.getZ() < 30000000;
     }
 
 }
