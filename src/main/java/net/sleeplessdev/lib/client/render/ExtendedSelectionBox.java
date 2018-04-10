bfccf6a6bbca30dbcc23147141fe4ef75d5c0179
InsomniaKitten <admin@designdevils.co.uk>
2018-04-10 16:53:33 +0100

    Refactor selection box implementation  - Batch all selection box vertices to the buffer before drawing  - Pass hit coordinates to the collection method for extra function

--- src/main/java/net/sleeplessdev/lib/client/render/ExtendedSelectionBox.java
+++ src/main/java/net/sleeplessdev/lib/client/render/ExtendedSelectionBox.java
@@ -3,12 +3,19 @@ package net.sleeplessdev.lib.client.render;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.IBlockAccess;
 
 import java.util.List;
 
 public interface ExtendedSelectionBox {
 
+    @Deprecated // Use new method below that passes hit vector coordinates
     void getSelectionBoundingBoxes(IBlockState state, IBlockAccess world, BlockPos pos, List<AxisAlignedBB> boxes);
+
+    @SuppressWarnings("deprecation")
+    default void getSelectionBoundingBoxes(IBlockState state, IBlockAccess world, BlockPos pos, Vec3d hitVec, List<AxisAlignedBB> boxes) {
+        getSelectionBoundingBoxes(state, world, pos, boxes);
+    }
 
 }
--- src/main/java/net/sleeplessdev/lib/internal/event/RenderEvents.java
+++ src/main/java/net/sleeplessdev/lib/internal/event/RenderEvents.java
@@ -1,15 +1,18 @@
 package net.sleeplessdev.lib.internal.event;
 
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.GlStateManager.DestFactor;
 import net.minecraft.client.renderer.GlStateManager.SourceFactor;
 import net.minecraft.client.renderer.RenderGlobal;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.RayTraceResult;
-import net.minecraft.world.World;
+import net.minecraft.util.math.RayTraceResult.Type;
+import net.minecraft.util.math.Vec3d;
 import net.minecraftforge.client.event.DrawBlockHighlightEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -32,45 +35,49 @@ final class RenderEvents {
     @SubscribeEvent
     protected static void onDrawBlockHighlight(DrawBlockHighlightEvent event) {
         if (event.getTarget() == null) return;
-        if (event.getTarget().typeOfHit != RayTraceResult.Type.BLOCK) return;
+        if (event.getTarget().typeOfHit != Type.BLOCK) return;
 
-        EntityPlayer player = event.getPlayer();
-        World world = player.world;
-        BlockPos pos = event.getTarget().getBlockPos();
-        IBlockState state = world.getBlockState(pos);
+        final EntityPlayer player = event.getPlayer();
+        final BlockPos pos = event.getTarget().getBlockPos();
+        final IBlockState state = player.world.getBlockState(pos);
 
         if (!(state.getBlock() instanceof ExtendedSelectionBox)) return;
 
-        state = state.getActualState(world, pos);
+        event.setCanceled(true);
 
-        ExtendedSelectionBox iface = (ExtendedSelectionBox) state.getBlock();
-        List<AxisAlignedBB> boxes = new ArrayList<>();
+        final ExtendedSelectionBox iface = (ExtendedSelectionBox) state.getBlock();
+        final List<AxisAlignedBB> boxes = new ArrayList<>();
+        final Vec3d hitVec = event.getTarget().hitVec.subtract(
+                pos.getX(), pos.getY(), pos.getZ()
+        );
 
-        iface.getSelectionBoundingBoxes(state, world, pos, boxes);
+        iface.getSelectionBoundingBoxes(state, player.world, pos, hitVec, boxes);
 
-        double offsetX = player.lastTickPosX + (player.posX - player.lastTickPosX) * event.getPartialTicks();
-        double offsetY = player.lastTickPosY + (player.posY - player.lastTickPosY) * event.getPartialTicks();
-        double offsetZ = player.lastTickPosZ + (player.posZ - player.lastTickPosZ) * event.getPartialTicks();
+        if (boxes.isEmpty()) return;
 
-        GlStateManager.pushMatrix();
-        GlStateManager.disableAlpha();
         GlStateManager.enableBlend();
         GlStateManager.tryBlendFuncSeparate(
                 SourceFactor.SRC_ALPHA, DestFactor.ONE_MINUS_SRC_ALPHA,
-                SourceFactor.ONE, DestFactor.ZERO);
+                SourceFactor.ONE, DestFactor.ZERO
+        );
         GlStateManager.glLineWidth(2.0F);
         GlStateManager.disableTexture2D();
         GlStateManager.depthMask(false);
 
-        double x = pos.getX() - offsetX;
-        double y = pos.getY() - offsetY;
-        double z = pos.getZ() - offsetZ;
+        final float ticks = event.getPartialTicks();
+        final double x = pos.getX() - (player.lastTickPosX + ((player.posX - player.lastTickPosX) * ticks));
+        final double y = pos.getY() - (player.lastTickPosY + ((player.posY - player.lastTickPosY) * ticks));
+        final double z = pos.getZ() - (player.lastTickPosZ + ((player.posZ - player.lastTickPosZ) * ticks));
+
+        final Tessellator tessellator = Tessellator.getInstance();
+        final BufferBuilder buffer = tessellator.getBuffer();
 
         GlStateManager.translate(x + 0.5D, y + 0.5D, z + 0.5D);
 
         if (iface instanceof OrientableSelectionBox) {
-            OrientableSelectionBox orientable = (OrientableSelectionBox) iface;
-            Vector3f angle = orientable.getRotationAngles(state, world, pos);
+            // TODO Migrate to matrix transformations and pass data into buffer builder
+            final OrientableSelectionBox oiface = (OrientableSelectionBox) iface;
+            final Vector3f angle = oiface.getRotationAngles(state, player.world, pos);
             GlStateManager.rotate(angle.x, 1.0F, 0.0F, 0.0F);
             GlStateManager.rotate(angle.y, 0.0F, 1.0F, 0.0F);
             GlStateManager.rotate(angle.z, 0.0F, 0.0F, 1.0F);
@@ -78,18 +85,23 @@ final class RenderEvents {
 
         GlStateManager.translate(-0.5D, -0.5D, -0.5D);
 
+        buffer.begin(3, DefaultVertexFormats.POSITION_COLOR);
+
         for (AxisAlignedBB box : boxes) {
-            RenderGlobal.drawSelectionBoundingBox(box.grow(0.002D), 0.0F, 0.0F, 0.0F, 0.4F);
+            final double minX = box.minX - 0.002D;
+            final double minY = box.minY - 0.002D;
+            final double minZ = box.minZ - 0.002D;
+            final double maxX = box.maxX + 0.002D;
+            final double maxY = box.maxY + 0.002D;
+            final double maxZ = box.maxZ + 0.002D;
+            RenderGlobal.drawBoundingBox(buffer, minX, minY, minZ, maxX, maxY, maxZ, 0.0F, 0.0F, 0.0F, 0.4F);
         }
+
+        tessellator.draw();
 
         GlStateManager.depthMask(true);
         GlStateManager.enableTexture2D();
-        GlStateManager.glLineWidth(0.2F);
         GlStateManager.disableBlend();
-        GlStateManager.enableAlpha();
-        GlStateManager.popMatrix();
-
-        event.setCanceled(true);
     }
 
 }
